79ee580432ec84fa2ed707ee01671293a9305726
Vojtech Mašek <shooter.vm@gmail.com>
2015-11-19 05:14:08 +0100

    Changes before splitting

--- isa-project.py
+++ isa-project.py
@@ -4,7 +4,7 @@
 from pprint import pprint  # only for debug and test reasons, to pretty print objects and lists
 from urllib import request
 from isaCommon import log, error
-from isaConnector import connect_to_tracker
+from isaConnector import connect_to_http_tracker, connect_to_udp_tracker
 import isaCommon
 import os
 import shutil
@@ -80,18 +80,21 @@ def open_gzipped_file(name) -> bytes:
 def parse_torrent(torrent) -> dict:
     # Decoded torrent_metadata object contains b'value' as binary data, that is why we need use it explicitly sometimes,
     # otherwise it wont match the values
+    # Also this contains additional items that will be used for connection to announce
     torrent_data = {'metadata': bencodepy.decode(torrent), 'trackers': {'http': [], 'udp': []}, 'info_hash': '',
-                    'hex_info_hash': '', 'peer_id': "PY3-ISA-project-2015"}
+                    'hex_info_hash': '', 'peer_id': "PY3-ISA-project-2015",
+                    'uploaded': 0, 'downloaded': 0, 'left': 1000, 'compact': 1, 'port': 6886, 'numwant': -1,
+                    }
 
     # if 'announce-list' keyword is missing in list, there is only one tracker for this torrent available
     # and therefor 'announce' will be used to connect to the torrent tracker
     if b'announce-list' in torrent_data['metadata']:
         log("announce-list keyword is present, there are more possible trackers", 2)
-        usable_trackers(torrent_data['metadata'][b'announce-list'], torrent_data['trackers'])
+        split_trackers(torrent_data['metadata'][b'announce-list'], torrent_data['trackers'])
     else:
         log("announce-list keyword is NOT present in dictionary, only one choice for tracker", 2)
         # here will be value under keyword announce used as only tracker
-        usable_trackers(torrent_data['metadata'][b'announce'], torrent_data['trackers'])
+        split_trackers(torrent_data['metadata'][b'announce'], torrent_data['trackers'])
 
     torrent_data['info_hash'] = get_info_hash(torrent_data['metadata'][b'info'])
     torrent_data['hex_info_hash'] = get_info_hash(torrent_data['metadata'][b'info'], True)
@@ -99,14 +102,14 @@ def parse_torrent(torrent) -> dict:
     return torrent_data
 
 
-def usable_trackers(tracker, trackers):
+def split_trackers(tracker, trackers):
     # Due to extend of standard list of backups can be passed as single tracker, this will check them all one by one
     # to see if there is any of http ones
     if isinstance(tracker, list):
         # Iterate through the all backups of the tracker or through the all trackers in the list, if more than one
         # tracker is passed (announce-list was present in torrent file metadata)
         for backup in tracker:
-            usable_trackers(backup, trackers)
+            split_trackers(backup, trackers)
     else:
         if tracker[:4] == b'http':
             trackers['http'].append(tracker.decode("utf-8"))
@@ -124,7 +127,7 @@ def get_info_hash(metadata, hex_hash=False) -> str:
         return hashlib.sha1(bencodepy.encode(metadata)).digest()
 
 
-def get_peers_for_torrent(torrent) -> list:
+def get_peers_for_torrent(torrent):
     torrent_data = parse_torrent(torrent)
     # announce = torrent_data['trackers']['http'][2]
     peers = []
@@ -136,11 +139,14 @@ def get_peers_for_torrent(torrent) -> list:
         log("Getting peers for announce %s" % announce, 1)
         peers.extend(get_peers_from_tracker(announce, torrent_data))
 
+    peers.extend(get_peers_from_tracker("", torrent_data, http=False))  # should be get_peers_from_tracker(announce, torrent_data)
+
     write_peers_to_file(torrent_data['hex_info_hash'], peers)
 
 
-def get_peers_from_tracker(announce, torrent_data) -> list:
-    response = connect_to_tracker(announce, torrent_data)
+def get_peers_from_tracker(announce, torrent_data, http=True) -> list:
+    response = connect_to_http_tracker(announce, torrent_data) if http else connect_to_udp_tracker(
+        torrent_data['trackers']['udp'][0], torrent_data)
     if response == b'':
         log('Response of tracker was empty', 2)
         return []  # if error has occurred empty list of peers will be returned
@@ -191,16 +197,18 @@ def write_peers_to_file(name, peers):
 
 
 def start():
-    torrent = download_torrent(
-        "http://torcache.net/torrent/3F19B149F53A50E14FC0B79926A391896EABAB6F.torrent?title=[kat.cr]ubuntu.15.10.desktop.64.bit")
-
-    # torrent = open_torrent('/home/shooter/PROJECTS/SCHOOL/ISA-project/[kat.cr]ubuntu.15.10.desktop.64.bit.torrent')
+    # url = "http://torcache.net/torrent/3F19B149F53A50E14FC0B79926A391896EABAB6F.torrent?title=[kat.cr]ubuntu.15.10.desktop.64.bit"
+    # torrent = download_torrent(url)
+    torrent = open_torrent('/home/shooter/PROJECTS/SCHOOL/ISA-project/[kat.cr]ubuntu.15.10.desktop.64.bit.torrent')
 
     get_peers_for_torrent(torrent)
 
 
 if __name__ == '__main__':
-    isaCommon.SHOW_FLAG = 1  # ALL LOGs WILL BE DISPLAYED
+    # max log deep is 3, if you choose 3 or more ALL LOGs WILL BE DISPLAYED
+    # if -1 there will be NO LOGs at all, and 0 is displaying only non breaking error logs
+    isaCommon.SHOW_FLAG = 5
+
     try:
         start()
     except KeyboardInterrupt:
